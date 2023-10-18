1472653a12d16df607f70ef5e8d0eb03faeb7797
GusMenezes <gustavoalvesmenezes@gmail.com>
2023-10-18 17:35:28 -0300

    up

--- Versao_Json/scrapy_byjson.py
+++ Versao_Json/scrapy_byjson.py
@@ -1,25 +1,110 @@
 import requests
+import time ,re
+import pandas as pd
 from bs4 import BeautifulSoup
 from lxml.html import fromstring
 import json
 
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-resposta = requests.get('https://www.vivareal.com.br/imovel/apartamento-2-quartos-tubalina-bairros-uberlandia-com-garagem-68m2-venda-RS480000-id-2660981065/',headers=headers)
+
+lista_url = []
+with open("Lista_Urls.txt","r") as f:
+    urls = f.read().split("\n")
+    for u in urls:
+        lista_url.append(u)
+
+lista_imoveis = []
 
 
-soup = fromstring(resposta.content)
+for i in range(len(lista_url)):
+    #Request site imovel
+    resposta = requests.get(lista_url[i],headers=headers)
 
-script = soup.xpath('/html/body/script[1]/text()')
+    soup = fromstring(resposta.text)
 
-script = str(script[0])
+    script = soup.xpath('/html/body/script[1]/text()')
 
-js = script.split('/* Other data */\n      ...')
-js = js[1].split('\n    };\n\n  function getSettings()')
-js = js[0]
+    script = str(script[0])
 
-json_object = json.loads(js.strip("()"))
+    js = script.split('/* Other data */\n      ...')
+    js = js[1].split('\n    };\n\n  function getSettings()')
+    js = js[0]
+
+    json_object = json.loads(js.strip("()"))
+
+    id = json_object.get('listing').get('id')
+    unitType = json_object.get('listing').get('unitTypes')[0]
+    listingType = json_object.get('listing').get('listingType')
+    state = json_object.get('listing').get('address').get('state')
+    city = json_object.get('listing').get('address').get('city')
+    neighborhood = json_object.get('listing').get('address').get('neighborhood')    
+    street = json_object.get('listing').get('address').get('street')
+    streetNumber = json_object.get('listing').get('address').get('streetNumber')
+    zipcode = json_object.get('listing').get('address').get('zipCode')
+    title = json_object.get('listing').get('title')
+    externalId = json_object.get('listing').get('externalId')
+    price = json_object.get('listing').get('pricingInfos')[0].get('price')
+    iptu = json_object.get('listing').get('pricingInfos')[0].get('yearlyIptu')
+    condominiumFee = json_object.get('listing').get('pricingInfos')[0].get('monthlyCondoFee')
+    usableAreas = json_object.get('listing').get('usableAreas')[0]
+    bedrooms = json_object.get('listing').get('bedrooms')[0]
+    bathrooms = json_object.get('listing').get('bathrooms')[0]
+    print(json_object.get('listing').get('suites') )
+    if len(json_object.get('listing').get('suites'))==1:
+        suites = json_object.get('listing').get('suites')[0]
+    else:
+        suites = ''
+    if len(json_object.get('listing').get('parkingSpaces'))==1:
+        parkingSpaces = json_object.get('listing').get('parkingSpaces')[0]
+    else:
+        parkingSpaces = ''
+    status = json_object.get('listing').get('status')
+    amenties = json_object.get('listing').get('amanties')
+    description = json_object.get('listing').get('description')
+    createdAt = json_object.get('listing').get('createdAt')
+    floors = json_object.get('listing').get('floors')
+    advertiserName = json_object.get('account').get('name')
+    advertiserLicenseNumber = json_object.get('account').get('licenseNumber')
+    advertiserPhonePrimary = json_object.get('account').get('phone').get('primary')
+    advertiserPhoneMobile = json_object.get('account').get('phone').get('mobile')
+    urlImovel = lista_url[i]
+
+    data = {
+        'ID_VivaReal':id,
+        'UnitType':unitType,
+        'ListingType':listingType,
+        'State':state,
+        'City':city,
+        'Neighborhood':neighborhood,
+        'Street':street,
+        'StreetNumber':streetNumber,
+        'ZipCode':zipcode,
+        'Title':title,
+        'ExternalId':externalId,
+        'Price':price,
+        'Iptu':iptu,
+        'CondominiumFee':condominiumFee,
+        'UsableAreas':usableAreas,
+        'Bedrooms':bedrooms,
+        'Bathrooms':bathrooms,
+        'Suites':suites,
+        'ParkingSpaces':parkingSpaces,
+        'Status':status,
+        'Amenties':amenties,
+        'Description':description,
+        'CreatedAt':createdAt,
+        'Floors':floors,
+        'Advertiser_Name':advertiserName,
+        'Advertiser_License':advertiserLicenseNumber,
+        'Advertiser_Phone_Primary':advertiserPhonePrimary,
+        'Advertiser_Phone_Mobile':advertiserPhoneMobile,
+        'Url_Place':urlImovel
+    }
+    
+    lista_imoveis.append(data)
 
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(json_object, f, ensure_ascii=False, indent=5)
+df = pd.DataFrame(lista_imoveis)
+df = df.drop_duplicates()
+df.to_csv('Exel_and_Csv_Files\\teste.csv',index=False)
--- WebScraping_VivaReal_part1.py
+++ WebScraping_VivaReal_part1.py
@@ -10,17 +10,17 @@ headers = {'user-agent': 'Mozilla/5.0'}
 options = Options()
 #options.add_argument('--headless')
 options.add_argument('window-size=1366,768')
-
+options.add_argument("user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 
 navegador = webdriver.Chrome(options=options)
 navegador.get('https://www.vivareal.com.br/venda/minas-gerais/uberlandia/apartamento_residencial/#onde=Brasil,Minas%20Gerais,Uberl%C3%A2ndia,,,,,,BR%3EMinas%20Gerais%3ENULL%3EUberlandia,,,')
+time.sleep(3)
+
+lista_url = []
 
 
-lista_imoveis = []
-
-
-pages = True
-while pages == True:
+pages = 0
+while pages <= 2:
     try:    
         
         site = BeautifulSoup(navegador.page_source,'html.parser')
@@ -29,90 +29,15 @@ while pages == True:
         imoveis = site.find_all('div',attrs={"data-type":"property"})
 
         for iv in imoveis:
-            
-            #Coleta id VivaReal
-            if iv.get('id') is not None:   
-                full_id = iv.get('id')
-                id_iv = full_id
-            else:
-                full_id = iv.find('div',id = True).get('id')
-                id_iv = full_id
-
-            
+           
             #Coleta Url imovel
             full_url = iv.find(class_='property-card__main-info').a.get('href')
             full_url = 'https://www.vivareal.com.br'+full_url
             url_iv = full_url
 
-            
-            #Coleta endereço e bairro
-            full_endereco = iv.find('span',class_="property-card__address").text.strip()
-            endereco = full_endereco.replace('\n', '')
-            if full_endereco[:3]=='Rua' or full_endereco[:7]=='Avenida' or full_endereco[:8]=='Travessa' or full_endereco[:7]=='Alameda' or full_endereco[:5]=='Praça':
-                bairro_1=full_endereco.strip().find('-')
-                bairro_2=full_endereco.strip().find(',', bairro_1)
-                if bairro_2!=-1:
-                    bairro_text=full_endereco.strip()[bairro_1+2:bairro_2]
-                    bairro = bairro_text # Guarde na lista todos os bairros
-                else: # Bairro não encontrado
-                    bairro_text='-'
-                    bairro = bairro_text # Caso o bairro não seja encontrado
-            else:
-                get_comma=full_endereco.find(',')
-                if get_comma!=-1:
-                    bairro_text=full_endereco[:get_comma]
-                    bairro = bairro_text # Guarde na lista todos os bairros com problema de formatação provenientes do proprio website  
-                else:
-                    get_hif=full_endereco.find('-')
-                    bairro_text=full_endereco[:get_hif]
-                    bairro = bairro_text
-
-
-            #Coleta a area
-            full_area=iv.find(class_="property-card__detail-value js-property-card-value property-card__detail-area js-property-card-detail-area").text.strip()
-            area = full_area
-
-            
-            # Coleta titulo
-            full_title = iv.find('span',class_='property-card__title js-cardLink js-card-title').text.strip()
-            full_title = full_title.replace('\n','')
-            title = full_title
-            
-            
-            # Coleta numero de quartos
-            full_quartos=iv.find(class_="property-card__detail-item property-card__detail-room js-property-detail-rooms").text.strip()
-            full_quartos=full_quartos.replace(' ','')
-            full_quartos=full_quartos.replace('\n','')
-            full_quartos=full_quartos.replace('Quartos','')
-            full_quartos=full_quartos.replace('Quarto','')
-            quartos = full_quartos
-
-            
-            # Coleta numero de banheiros
-            full_banheiros=iv.find(class_="property-card__detail-item property-card__detail-bathroom js-property-detail-bathroom").text.strip()        
-            full_banheiros=full_banheiros.replace(' ','')
-            full_banheiros=full_banheiros.replace('\n','')
-            full_banheiros=full_banheiros.replace('Banheiros','')
-            full_banheiros=full_banheiros.replace('Banheiro','')
-            banheiros = full_banheiros  
-
-            
-            # Coleta numero de vagas de garagem
-            full_garagem=iv.find(class_="property-card__detail-item property-card__detail-garage js-property-detail-garages").text.strip()        
-            full_garagem=full_garagem.replace(' ','')
-            full_garagem=full_garagem.replace('\n','')
-            full_garagem=full_garagem.replace('Vagas','')
-            full_garagem=full_garagem.replace('Vaga','')
-            garagens = full_garagem 
-
-            
-            #Coleta preço
-            full_preco = re.sub('[^0-9]','',iv.find(class_="property-card__price js-property-card-prices js-property-card__price-small").text.strip())
-            preco = full_preco
-
-            #                       0     1    2   3    4     5        6     7      8        9     10     11    12  13  14  15  16  17    18               
-            lista_imoveis.append([id_iv,preco,'-','-',title,bairro,endereco,area,quartos,banheiros,'-',garagens,'-','-','-','-','-','-',url_iv])
-
+            lista_url.append(url_iv)
+        
+        pages +=1
 
     except:
         print('Erro, 10 min para retomada')
@@ -142,7 +67,10 @@ while pages == True:
 #fecha o chromedriver
 navegador.quit()
 
-#                                           0       1         2          3      4         5        6        7        8         9         10         11         12           13          14                  15                16             17            18                                           
-df = pd.DataFrame(lista_imoveis,columns=['ID_VR','Preço','Condominio','IPTU','Titulo','Bairro','Endereco','Area','Quartos','Banheiros','Suites','Garagens','Anunciante','Telefone','Cod.Imobiliaria','Disponibilidade','Caracteristicas','Descrição','Url_Imovel'])
-df =df.drop_duplicates()
-df.to_csv('Exel_and_Csv_Files\Viva_Real_Scrap.csv',index=False)
+
+with open('Lista_Urls.txt', 'w') as arquivo:
+    for i in range(0,len(lista_url)):
+        if i==len(lista_url)-1:
+            arquivo.write(str(lista_url[i]))
+        else:
+            arquivo.write(str(lista_url[i])+'\n')
